6c43c106f6dccf47c00354400f36f3a9780c32ff
Marcel Schleinkofer <marcel.schleinkofer@innotix.com>
2024-02-04 15:25:14 +0100

    api setup

--- vueapp/vite.config.ts
+++ vueapp/vite.config.ts
@@ -12,5 +12,38 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:7071',
+        ws: true,
+        changeOrigin: true,
+      },
+      // '/api': {
+      //   target: 'http://localhost:7071',
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/api/, ''),
+      // },
+      // // with RegEx: http://localhost:5173/fallback/ -> http://jsonplaceholder.typicode.com/
+      // '^/fallback/.*': {
+      //   target: 'http://jsonplaceholder.typicode.com',
+      //   changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/fallback/, ''),
+      // },
+      // // Using the proxy instance
+      // '/api': {
+      //   target: 'http://jsonplaceholder.typicode.com',
+      //   changeOrigin: true,
+      //   configure: (proxy, options) => {
+      //     // proxy will be an instance of 'http-proxy'
+      //   },
+      // },
+      // // Proxying websockets or socket.io: ws://localhost:5173/socket.io -> ws://localhost:5174/socket.io
+      // '/socket.io': {
+      //   target: 'ws://localhost:5174',
+      //   ws: true,
+      // },
+    },
+  },
 })
